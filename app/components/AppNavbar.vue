967304ab34bc2372080816a387db7d02e5e85f69
Nicolas Knittel <ohsiixo@gmail.com>
2025-09-12 01:03:23 +0200

    Navbar

--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -1,18 +1,34 @@
 <template>
   <nav
-    class="flex justify-between items-center w-full px-4 py-2 bg-black text-white border-b-1 border-white"
+    class="flex justify-between items-center w-full bg-gray-900 text-white border-b-1 border-white p-4"
   >
-    <div class="flex gap-4">
-      <ul class="flex gap-4">
-        <li class="border-r-1 border-white p-2">
-          <h1>Cours</h1>
-        </li>
-        <li>
-          <h1>Exercices</h1>
-        </li>
-      </ul>
+    <div class="font-bold text-xl">
+      <NuxtLink
+        to="/"
+        class="bg-white text-black p-4 rounded-2xl hover:bg-black hover:text-white"
+        >APP</NuxtLink
+      >
     </div>
-
+    <div class="flex text-lg gap-4">
+      <NuxtLink
+        to="/"
+        class="p-2 flex items-center hover:bg-white hover:text-black rounded-2xl"
+      >
+        Accueil
+      </NuxtLink>
+      <NuxtLink
+        to="/Cours"
+        class="p-2 flex items-center hover:bg-white hover:text-black rounded-2xl"
+      >
+        Cours
+      </NuxtLink>
+      <NuxtLink
+        to="/Exercices"
+        class="p-2 flex items-center hover:bg-white hover:text-black rounded-2xl"
+      >
+        Exercices
+      </NuxtLink>
+    </div>
     <div>
       <NuxtLink
         to="https://github.com/nuxt/ui"
